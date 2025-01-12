f2f389aa96d3330550ffd7eb91bbfdc947b231c5
ikanueveuax <ikarakul@myuax.com>
2025-01-12 15:38:03 +0100

    Commit inicial

---
+++ src/test/java/com/gestionpedidos/feedback1gestionpedidos/Feedback1GestionPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionpedidos.feedback1gestionpedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Feedback1GestionPedidosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
