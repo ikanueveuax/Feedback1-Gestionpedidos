67607dc6e3256774b29c3cdf68278cb81e3bbf11
ikanueveuax <ikarakul@myuax.com>
2025-01-12 15:38:03 +0100

    Commit inicial

---
+++ src/main/java/com/gestionpedidos/feedback1gestionpedidos/Feedback1GestionPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.gestionpedidos.feedback1gestionpedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Feedback1GestionPedidosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Feedback1GestionPedidosApplication.class, args);
+    }
+
+}
